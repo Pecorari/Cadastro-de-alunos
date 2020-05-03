ffa01c7515d92ec639f16fbcb8f0f3b52979a56b
Thiago Pecorari Clemente <thiagojapa2003@hotmail.com>
2020-05-03 11:53:24 -0300

    'first-commit'

---
+++ src/pages/Login/index.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+import { Container } from '../../styles/GlobalStyle';
+import { Title } from './styled';
+
+function Login() {
+  return (
+    <Container>
+      <Title isRed>Lorem ipsum dolor sit amet.</Title>
+      <button type="button">Exemplo</button>
+    </Container>
+  );
+}
+
+export default Login;
